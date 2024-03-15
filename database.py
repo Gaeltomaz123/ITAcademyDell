26cb1789189cf06e08a0e3d3693e607bc919d1ad
Gaeltomaz123 <gaeltomaz@gmail.com>
2024-03-15 20:19:04 -0300

    fixing db

--- database.py
+++ database.py
@@ -10,32 +10,40 @@ class User(Model):
     class Meta:
         database = db
 
-class Numbers(Model):
-    user = ForeignKeyField(User, backref="users")
+class Draw_Price(Model):
+    rounds = IntegerField()
     first = IntegerField()
     second = IntegerField()
     third = IntegerField()
     fourth = IntegerField()
     fifth = IntegerField()
-
     class Meta:
         database = db
 
 class Rounds(Model):
     round_number = IntegerField()
-    numbers = ManyToManyField(Numbers, backref="numbers")
+    draw_price = ForeignKeyField(Draw_Price, backref="draw_price")
+    has_winner = BooleanField(default=False)
 
     class Meta:
         database = db
 
-class Draw_Price(Model):
-    rounds = ForeignKeyField(Rounds, backref="rounds")
-    winners = ManyToManyField(User, backref="users")
+class Bet(Model):
+    user = ForeignKeyField(User, backref="users")
+    draw_price = ForeignKeyField(Draw_Price, backref="draw_price")
+    round_number = ForeignKeyField(Rounds.round_number, backref="round")
     first = IntegerField()
     second = IntegerField()
     third = IntegerField()
     fourth = IntegerField()
     fifth = IntegerField()
+
+    class Meta:
+        database = db
+
+class Draw_Price_Winners_Relationship(Model):
+    draw_price = ForeignKeyField(Draw_Price, backref="draw_price")
+    user = ForeignKeyField(User, backref="users")
 
     class Meta:
         database = db
